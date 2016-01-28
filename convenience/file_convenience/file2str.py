04a91589cf30c49785d058e5d05c96fc2f033929
ronrest <ronny.coding@gmail.com>
2016-01-28 21:33:41 +1100

    Add docstring to file2str()

--- convenience/file_convenience/file2str.py
+++ convenience/file_convenience/file2str.py
@@ -1,4 +1,15 @@
+# ==============================================================================
+#                                                                       FILE2STR
+# ==============================================================================
 def file2str(file):
+    """
+    Takes a file path and returns the contents of that file as a tring object.
+
+    :param file: (string)
+        The path to the file.
+    :return:
+    """
+    # ==========================================================================
     with open(file, "r") as textFile:
         text = textFile.read()
     return text
