385a58a719687c4c3c64a2c966a0c389d6514568
ronrest <ronny.coding@gmail.com>
2016-01-29 21:35:16 +1100

    Add docstring to un_normalise_columns()

--- convenience/ml/normalise.py
+++ convenience/ml/normalise.py
@@ -29,5 +29,28 @@ def normalise_columns(x, mean, sd):
     return (np.array(x) - mean) / sd
 
 
+
+#===============================================================================
+#                                                           UN_NORMALISE COLUMNS
+#===============================================================================
 def un_normalise_columns(z, mean, sd):
+    """
+    Takes a 2D array of Z-score values, along with the mean and standard
+    deviations for each column used to calculate those z-scores. Returns the
+    values in their original scale.
+
+    :param z: (2D Array)
+        A 2D array, containing z-score values
+    :param mean: (1D array)
+        The means that were used to normalise each of the columns of z
+    :param sd: (1D array)
+        The standard deviations that were used to normalise each of the columns
+        of z
+    :return: (numpy array)
+        Returns an array where the elements are the values in their original
+        scale. .
+    """
+    # ==========================================================================
     return z*sd + mean
+
+
