4d2953bc54bf6adf33eb935fc399ebab807d1c5c
ronrest <ronny.coding@gmail.com>
2016-01-30 21:19:50 +1100

    Add Docstring to tally()

--- convenience/ml/tally.py
+++ convenience/ml/tally.py
@@ -1,7 +1,25 @@
 import numpy as np
 import pandas as pd
 
+#===============================================================================
+#                                             Tally, and Ratios of Unique Values
+#===============================================================================
 def tally(x, as_df=True):
+    """
+    Takes an array, and returns a table where the first column gives the unique
+    values. The second column gives the counts of each of those values. And the
+    third column gives the ratio proportions of each of the values.
+
+    :param x: the numpy array
+    :param as_df: {boolean} {Default = True}
+
+        If True, it returns a pandas Dataframe with labelled columns
+
+        If False, it returns a 2D numpy array.
+
+    :return: {2D array or pandas Dataframe}
+    """
+    # ==========================================================================
     cc =np.array(np.unique(x, return_counts=True))
     ratios = cc[1] / float(cc[1].sum())
     table = np.vstack((cc, ratios)).T
@@ -11,4 +29,5 @@ def tally(x, as_df=True):
         return table
     else:
         return table
+
 
