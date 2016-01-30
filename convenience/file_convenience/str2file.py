fdbbc74500c03161b9a0e2394e274b8b8a0fa72b
ronrest <ronny.coding@gmail.com>
2016-01-30 21:23:18 +1100

    Add docstring to str2file()

--- convenience/file_convenience/str2file.py
+++ convenience/file_convenience/str2file.py
@@ -1,4 +1,32 @@
+# ==============================================================================
+#                                                                       STR2FILE
+# ==============================================================================
 def str2file(s, file, append=True, sep="\n"):
+    """
+    Takes a string object and a path to a file, and saves the contents of the
+    string as text in the file. If the file does not already exist, a new file
+    is created automatically.
+
+    If the file already exists, then you can chose whether you want to append
+    the new text to the file (this is the default behaviour), or replace the
+    file altogether with the new content.
+
+    If you chose to append, you can also chose how the new content gets
+    separated from the existing content. By default, new content appears on a
+    new line. But you could chose any other other string to separate the content
+    or a blank string to make no separation at all.
+
+    :param s: (string)
+        The string containing the new content
+    :param file: (string)
+        The path to the file
+    :param append: (bool)(default = True)
+        Should it append new content to existing file? (or replace the file with
+        new content)
+    :param sep: (string)(default = "\n")
+        The string used to separate the existing content with the new content.
+    """
+    # ==========================================================================
     mode = "a" if append else "w"    # Append or replace mode
     if append and (sep != ""):
         s = sep + s                  # Appended text separated by desired string
