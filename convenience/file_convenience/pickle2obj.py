e801b039c1a2e3c8b5aef4034b22278b928c735d
ronrest <ronny.coding@gmail.com>
2016-01-31 22:34:28 +1100

    Add docstring to pickle2obj()

--- convenience/file_convenience/pickle2obj.py
+++ convenience/file_convenience/pickle2obj.py
@@ -1,6 +1,22 @@
 import pickle
 
+# ==============================================================================
+#                                                                     PICKLE2OBJ
+# ==============================================================================
 def pickle2obj(file):
+    """
+    Takes a filepath to a picke object, and returns a python object specified
+    by that pickle file.
+
+    :param file: (str)
+        File path to a pickle file.
+    :return: (object)
+        A python object specified by the pickle file.
+
+    :example:
+        myObject = pickle2obj("/tmp/myPickle.pkl")
+    """
+    # ==========================================================================
     with open(file, mode = "rb") as fileObj:
         obj = pickle.load(fileObj)
     return obj
