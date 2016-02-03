f237b8d8464d9f70030c0d5e84519cfd28ff0078
ronrest <ronny.coding@gmail.com>
2016-02-03 22:13:00 +1100

    Add docstring to dict_file2tokenized_list()

--- convenience/nlp/dict_file2tokenized_list.py
+++ convenience/nlp/dict_file2tokenized_list.py
@@ -1,7 +1,36 @@
 from nltk import word_tokenize as tokenize
 
 
+# ==============================================================================
+#                                                       DICT_FILE2TOKENIZED_LIST
+# ==============================================================================
 def dict_file2tokenized_list(files, lower=True, encoding="utf-8"):
+    """
+    Takes a dictionary of file path lists, where each key of the dictionary
+    represents some category.
+
+    The output will be a tuple with 3 values.
+        tokenized_list = lists of tokenized strings.
+        labels         = a list containing integer labels corresponding to the
+                         category that each element of tokenized_list belongs to
+        cats           = A list of the unique category names. Indices of the
+                         names correspond to the integer values used in labels
+                         such that cats[labels[i]] gives you the original name
+                         for the category that the ith training example belongs
+                         to.
+
+    :param files: (str, or list of strings or dict)
+        String of a single file path, or a list of file path strings.
+    :param lower: (bool)(default = True)
+        Convert all text to lowercase?
+    :param encoding: (str)(default = "utf-8")
+        Encoding used in the text files
+    :return:
+        A tuple of 3 items if files is a dictionary containing lists of strings.
+        (see the description section for more details about the 3 elements
+        returned)
+    """
+    # ==========================================================================
     cats = files.keys()
     num_per_category  = {cat: len(files[cat]) for cat in cats}
     num_items = sum(num_per_category.values())
