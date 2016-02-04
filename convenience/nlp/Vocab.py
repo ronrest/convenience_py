d9794cd512b3b0265cc6f4f409563db0410b1cf8
ronrest <ronny.coding@gmail.com>
2016-02-04 22:18:16 +1100

    Add from_tokenized_lists() method in Vocab class

--- convenience/nlp/Vocab.py
+++ convenience/nlp/Vocab.py
@@ -7,3 +7,19 @@ class Vocab(object):
         self.i2w = Series() # index to word Series
         self.size = 0       # vocab size
 
+    def from_tokenized_lists(self, toklist):
+        print("Extracting the vocab from a tokenized list")
+        self.vocab = dict()
+        for sentence in toklist:
+            for word in sentence:
+                # If the word exists in wordcount, increment the value by 1. Otherwise
+                # create a new key, initialised to 0, and increment by 1.
+                self.vocab[word] = self.vocab.get(word, 0) + 1
+
+        self.vocab = Series(self.vocab)
+        self.vocab.sort_values(ascending=False, inplace=True)
+        self.vocab = concat([Series({u"UNKNOWN":0}), self.vocab], ignore_index=False)
+        self.w2i = Series(range(self.vocab.size), index=self.vocab.index)
+        self.i2w = self.vocab.index
+        self.size = self.vocab.size
+        print("---Done!")
