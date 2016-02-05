8b9424d7389e674e6a96a764a8942bcbbf520fa3
ronrest <ronny.coding@gmail.com>
2016-02-05 22:22:37 +1100

    Add import of Vocab() to the __init__ file

--- convenience/nlp/__init__.py
+++ convenience/nlp/__init__.py
@@ -7,3 +7,5 @@ __author__ = 'ronny'
 
 from file2tokenized_list import file2tokenized_list, file2tokenised_list
 from dict_file2tokenized_list import  dict_file2tokenized_list, dict_file2tokenised_list
+from Vocab import Vocab
+
