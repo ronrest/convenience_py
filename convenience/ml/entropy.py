7cff5a44aeaaf042808bd1342fa8f52963acf9de
ronrest <ronny.coding@gmail.com>
2016-02-10 22:10:40 +1100

    Add docstring to entropy()

--- convenience/ml/entropy.py
+++ convenience/ml/entropy.py
@@ -1,6 +1,17 @@
 import numpy as np
 
+# ==============================================================================
+#                                                                        ENTROPY
+# ==============================================================================
 def entropy(x):
+    """
+    Takes a 1D numpy array containing the counts for items for different
+    classes, and it returns the entropy.
+
+    :param x: (1D array)
+    :return: (float)
+    """
+    # ==========================================================================
     p = x/x.sum()                   # probability for each class
     return (-p*np.log2(p)).sum()    # return entropy
 
