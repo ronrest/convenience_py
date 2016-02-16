3af780172f065abdc504b6a16ca676dedc1473aa
ronrest <ronny.coding@gmail.com>
2016-02-16 19:57:50 +1100

    Add docstring to file_verification()

--- convenience/file_convenience/file_verification.py
+++ convenience/file_convenience/file_verification.py
@@ -1,6 +1,29 @@
 import hashlib
 
+# ==============================================================================
+#                                                              FILE_VERIFICATION
+# ==============================================================================
 def file_verification(file, v_type, v_val):
+    """
+    Verify the file using one of several methods including SHA1, SHA256, MD5,
+    filesize and others.
+
+    :param file: {string}
+        Filepath to the file
+    :param v_type: {string}
+        Method of verification to use.
+
+        "md5"
+        "sha1"
+        "sha256"
+        "sha512"
+        "bytes" # Not yet implemented
+
+    :param v_val: {string}
+        The value that it should be.
+    :return: {boolean}
+    """
+    # ==========================================================================
     # TODO: do dummy proofing for input values.
     v_types = {"md5": hashlib.md5,
                "sha1": hashlib.sha1,
