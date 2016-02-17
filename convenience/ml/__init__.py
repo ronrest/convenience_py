f4744e349c37a5714c83ae01f1b34f0c92d587ba
ronrest <ronny.coding@gmail.com>
2016-02-17 17:50:15 +1100

    Moved image2array() to be part of ml convenience instead of file convenience.

--- convenience/ml/__init__.py
+++ convenience/ml/__init__.py
@@ -9,5 +9,5 @@ from tally import tally
 from split_rows import split_rows, row_splitter_array
 from normalise import normalise_columns, un_normalise_columns
 from entropy import entropy
-
+from image2array import image2array
 
