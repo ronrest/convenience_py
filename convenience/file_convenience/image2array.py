c1b22b8d7811d98d0d3e0d2e0801bda42a61347d
ronrest <ronny.coding@gmail.com>
2016-02-17 17:47:12 +1100

    Add docstring to image2array()

--- convenience/file_convenience/image2array.py
+++ convenience/file_convenience/image2array.py
@@ -1,5 +1,20 @@
 from scipy import ndimage
 
+
+# ==============================================================================
+#                                                                    IMAGE2ARRAY
+# ==============================================================================
 def image2array(f):
+    """
+    Takes a file path to an image file, and returns the pixel data as a numpy
+    array of floats.
+
+    :param f: {string}
+        Filepath to the image file of interest.
+    :return: {numpy array}
+        Returns an array with the pixel data
+    """
+    # ==========================================================================
     return ndimage.imread(f).astype(float)
+
 
