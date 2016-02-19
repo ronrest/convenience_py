de4d8f523b36dee2080399b3e2ff040e35f4536e
ronrest <ronny.coding@gmail.com>
2016-02-20 10:50:30 +1100

    Add docstring to array2image()

--- convenience/ml/array2image.py
+++ convenience/ml/array2image.py
@@ -2,7 +2,17 @@ from matplotlib import pyplot as plt
 
 __author__ = 'ronny'
 
+# ==============================================================================
+#                                                                    ARRAY2IMAGE
+# ==============================================================================
 def array2image(x):
+    """
+    Takes a 2D array and converts it to an image.
+
+    :param x: {2D array}
+        The 2D array to create an image from.
+    """
+    # ==========================================================================
     plt.figure()
     plt.imshow(x, cmap="gray")
 
