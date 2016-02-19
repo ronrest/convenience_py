422f3a978abe58f072fe05a3734a1ccffae4a50e
ronrest <ronny.coding@gmail.com>
2016-02-20 10:44:21 +1100

    Add feature to flatten color channels of an image in image2array()

--- convenience/ml/image2array.py
+++ convenience/ml/image2array.py
@@ -4,17 +4,26 @@ from scipy import ndimage
 # ==============================================================================
 #                                                                    IMAGE2ARRAY
 # ==============================================================================
-def image2array(f):
+def image2array(f, flatten=False):
     """
     Takes a file path to an image file, and returns the pixel data as a numpy
     array of floats.
 
     :param f: {string}
         Filepath to the image file of interest.
+    :param flatten: {boolean}(default = False)
+        Should it flatten multiple color channels into one single channel?
+        - If True, then it takes the average of the color channels
     :return: {numpy array}
         Returns an array with the pixel data
     """
     # ==========================================================================
-    return ndimage.imread(f).astype(float)
+    img = ndimage.imread(f).astype(float)
+
+    # Flatten the image color channels if needed
+    if flatten and len(img.shape) > 2:
+        img = img.mean(axis=2)
+
+    return img
 
 
