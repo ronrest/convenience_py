0e34089ce30e37ce76cad8b406350b235dc6ea1b
ronrest <ronny.coding@gmail.com>
2016-02-20 10:58:30 +1100

    Add colormap option in array2image()

--- convenience/ml/array2image.py
+++ convenience/ml/array2image.py
@@ -5,16 +5,23 @@ __author__ = 'ronny'
 # ==============================================================================
 #                                                                    ARRAY2IMAGE
 # ==============================================================================
-def array2image(x):
+def array2image(x, cmap="gray"):
     """
     Takes a 2D array and converts it to an image.
 
     :param x: {2D array}
         The 2D array to create an image from.
+    :param cmap: {string} (default="gray")
+        Colormap to use. This value is passed on to  matplotlib.pyplot.imshow()
+        So whatever values are valid for that function can be used here.
+        Some possible values:
+            "gray"     = grayscale (default)
+            "spectral" = reverse rainbow heatmap from black to VIBGYOR to white
+            "hot"      = heatmap (black, red, yellow, white)
     """
     # ==========================================================================
     plt.figure()
-    plt.imshow(x, cmap="gray")
+    plt.imshow(x, cmap=cmap)
 
     plt.show()
 
