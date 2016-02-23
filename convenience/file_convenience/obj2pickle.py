1e1c44fb20d58d9aeb2db262ad95e53e4b9db303
ronrest <ronny.coding@gmail.com>
2016-02-23 21:07:28 +1100

    Add protocol option to obj2pickle()

--- convenience/file_convenience/obj2pickle.py
+++ convenience/file_convenience/obj2pickle.py
@@ -2,7 +2,7 @@ import pickle
 # ==============================================================================
 #                                                                     OBJ2PICKLE
 # ==============================================================================
-def obj2pickle(obj, file):
+def obj2pickle(obj, file, protocol=-1):
     """
     Saves an object as a binary pickle file to the desired file path.
 
@@ -10,6 +10,15 @@ def obj2pickle(obj, file):
         The python object you want to save.
     :param file: (str)
         File path of file you want to save as.  eg /tmp/myFile.pkl
+    :param protocol: (int) (default = -1)
+        0  = original ASCII protocol and is backwards compatible with earlier
+             versions of Python.
+        1  = old binary format which is also compatible with earlier versions
+             of Python.
+        2  = provides much more efficient pickling of new-style classes.
+             compatible with Python >= 2.3
+        -1 = uses the highest (latest) protocol available. This is the default
+             value for this function.
 
     :example:
         s = "This is my super important object i want to cache"
@@ -18,6 +27,6 @@ def obj2pickle(obj, file):
     # ==========================================================================
     print("pickling object to {} ".format(file))
     with open(file, mode="wb") as fileObj:
-        pickle.dump(obj, fileObj)
+        pickle.dump(obj, fileObj, protocol=protocol)
     print("---Done!")
 
