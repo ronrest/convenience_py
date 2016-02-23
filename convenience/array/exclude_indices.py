1a9ccabd787fcd3dbd3e23d2bbe679904ec9996f
ronrest <ronny.coding@gmail.com>
2016-02-23 16:25:33 +1100

    Fix bug, now exclude_indices() returns an array instead of list.

--- convenience/array/exclude_indices.py
+++ convenience/array/exclude_indices.py
@@ -16,13 +16,13 @@ def exclude_indices(x, excl):
     :param excl: {set, or 1-D array-like of integers}
         The indices you want to exclude frm x.
     :return:
-        returns x with the selected indices removed.
+        returns x as an array with the selected indices removed.
     :example:
         >>> a = np.array([11,12,13,14,15,16])
         >>> exclude_indices(a, {0, 3, 5})
-        [12, 13, 15]
+        array([12, 13, 15])
     """
     # ==========================================================================
-    return [x[i] for i in np.arange(len(x)) if i not in excl]
+    return np.array([x[i] for i in np.arange(len(x)) if i not in excl])
 
 
