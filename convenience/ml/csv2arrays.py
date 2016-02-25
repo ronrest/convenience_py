5e7b00f9dfd217a3907891ec1b4b47941414e993
ronrest <ronny.coding@gmail.com>
2016-02-26 10:44:21 +1100

    Add shuffle option in csv2arrays()

--- convenience/ml/csv2arrays.py
+++ convenience/ml/csv2arrays.py
@@ -6,7 +6,8 @@ __author__ = 'ronny'
 # ==============================================================================
 #                                                                     CSV2ARRAYS
 # ==============================================================================
-def csv2arrays(file, y_col=None, sep=",", skip_header=0, skip_footer=0,
+def csv2arrays(file, y_col=None, shuffle=False, sep=",", skip_header=0,
+               skip_footer=0,
                missing_values={"NA", "NAN", "N/A"}, filling_values=np.nan):
     """
     Takes a csv file and creates a tuple of arrays containing the data.
@@ -17,6 +18,8 @@ def csv2arrays(file, y_col=None, sep=",", skip_header=0, skip_footer=0,
     :param y_col: {int}(default=None)
         The column in the data containing the output labels. If this file
         doesnt contain any output labels, then use None.
+    :param shuffle: {boolean}(defualt=False)
+        Should it shuffle the order of the rows?
     :param sep: {str}(default=",")
         delimiter used to separate columns.
     :param skip_header: {int}(default=0)
@@ -39,13 +42,20 @@ def csv2arrays(file, y_col=None, sep=",", skip_header=0, skip_footer=0,
                          missing_values=missing_values,
                          filling_values=filling_values
                         )
+
+    #TODO: set seed
+
+    # Get the indices that would shuffle the data
+    row_indices = np.random.permutation(data.shape[0]) if shuffle \
+        else np.arange(data.shape[0])
+
     # Split into input data and output labels if there is an output column.
     if y_col is not None:
         x_cols = range(data.shape[1])
         x_cols.pop(y_col)
         X_train = data[:, x_cols]
         Y_train = data[:, y_col]
-        return X_train, Y_train
+        return X_train[row_indices,:], Y_train[row_indices]
     else:
-        return data
+        return data[row_indices,:]
 
