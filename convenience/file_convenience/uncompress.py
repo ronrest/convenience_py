6cb7d654c391e41060fdda3319ad6870cafad5df
ronrest <ronny.coding@gmail.com>
2016-02-29 21:55:21 +1100

    Add  docstring to uncompress()

--- convenience/file_convenience/uncompress.py
+++ convenience/file_convenience/uncompress.py
@@ -1,10 +1,27 @@
 
 
+# ==============================================================================
+#                                                                     UNCOMPRESS
+# ==============================================================================
 def uncompress(file, method="tar"):
+    """
+    Uncompress packaged files such as zip and tarballs.
+
+    :param file: {string}
+        filepath of compressed file
+    :param method: {string}
+        Method of uncompression to use.
+
+        TODO: autodetect based on filename.
+
+        "tar"  = for tar.gz files.
+
+    """
     #TODO: Add an output dir option.
     if method == "tar":
         import tarfile
         with tarfile.open(filename) as fileObj:
             #sys.stdout.flush()
             fileObj.extractall()
+
 
