c1fbf3e46b226e15120e6848e30aa923b346f64b
ronrest <ronny.coding@gmail.com>
2016-03-03 20:59:30 +1100

    plot_grid_array() - Add docstring

--- convenience/ml/plot_grid_array.py
+++ convenience/ml/plot_grid_array.py
@@ -4,7 +4,29 @@ import numpy as np
 __author__ = 'Ronny Restrepo'
 
 
+# ==============================================================================
+#                                                                PLOT_GRID_ARRAY
+# ==============================================================================
 def plot_grid_array(grid, cell_height, labels, title="", save=None, show=True):
+    """
+    Takes a 2D array that can be thought of as a grid of images, like the one
+    returned by the `sample_grid_of_arrays()` function, and plots it, with class
+    labels along the y axis for each row of images that belong to some class.
+
+    :param grid: {2D array}
+        The 2D array we are treating as a grid of arrays
+    :param cell_height: {int}
+        Height of each individual row
+    :param labels: {1D array-like}
+        Class labels to use on the plot.
+    :param title: {string}
+        Title for the plot.
+    :param save: {None of string}
+        filepath and name to save the plotted image as.
+    :param show:
+    :return:
+    """
+    # ==========================================================================
     plt.figure()
     plt.imshow(grid, cmap="gray")
 
