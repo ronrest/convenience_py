52d7fc9a0784900dfa28162d510eda64242f474f
ronrest <ronny.coding@gmail.com>
2016-03-06 19:40:39 +1100

    wrap_slice_indices() - Add docstring

--- convenience/array/wrap_slice.py
+++ convenience/array/wrap_slice.py
@@ -1,7 +1,42 @@
 
 
+# ==============================================================================
+#                                                             WRAP_SLICE_INDICES
+# ==============================================================================
 def wrap_slice_indices(array_len, start, slice_len):
+    """
+    Returns a list of indices that will allow you to make a slice of certain
+    size given some starting position.
+
+    If the combination of starting position and length of the slice go beyond
+    the boundaries of the array, then it automatically wraps around to the
+    beginning of the array and continues from there.
+
+    If the starting index is larger than the array length, then it calculates
+    the index it would start at if it were to wrap around the array as many
+    times as needed (Modular arithmetic)
+
+    See the examples for a better intuition.
+
+    :param array_len: {int}
+        The length of the array you want to slice.
+    :param start: {int}
+        The index you want to start at.
+    :param slice_len: {int}
+        How big do you want the slice?
+    :return: {list of integers}
+        A list of the indices that will allow you to slice the desired array.
+
+    :example:
+        >>> wrap_slice_indices(array_len=20, start=15, slice_len=10)
+        [15, 16, 17, 18, 19, 0, 1, 2, 3, 4]
+
+        >>> wrap_slice_indices(array_len=10, start=23, slice_len=3)
+        [3, 4, 5]
+    """
+    # ==========================================================================
     return [index % array_len for index in range(start, start+slice_len)]
+
 
 
 
@@ -45,4 +80,5 @@ def wrap_slice(x, start, slice_len):
     """
     # ==========================================================================
     return x[[index % x.shape[0] for index in range(start, start+slice_len)]]
+
 
