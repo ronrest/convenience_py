b47925df4aef4a0527deea66446746ddf47eaad1
ronrest <ronny.coding@gmail.com>
2016-03-08 22:03:38 +1100

    file_verification() - handle file size verification method

--- convenience/file_convenience/file_verification.py
+++ convenience/file_convenience/file_verification.py
@@ -1,4 +1,5 @@
 import hashlib
+from os import stat
 
 # ==============================================================================
 #                                                              FILE_VERIFICATION
@@ -17,30 +18,35 @@ def file_verification(file, v_type, v_val):
         "sha1"
         "sha256"
         "sha512"
-        "bytes" # Not yet implemented
+        "bytes"   = File size in bytes.
 
-    :param v_val: {string}
-        The value that it should be.
+    :param v_val: {string or int}
+        - If `v_val` is one of the hash types,then this should be a string with
+          the hash code that you expect the file to be.
+        - If `v_val` is "bytes" then this value should be an integer with the
+          filesize that you expect file to be.
     :return: {boolean}
+        Return True if the file matches the expected verification value.
     """
     # ==========================================================================
     # TODO: do dummy proofing for input values.
-    v_types = {"md5": hashlib.md5,
-               "sha1": hashlib.sha1,
-               "sha256": hashlib.sha256,
-               "sha512": hashlib.sha512
-               }
-    with open(file, 'rb') as fileObj:
-        content = fileObj.read()
-    hash = v_types[v_type](content).hexdigest()
 
-    # TODO: filesize verification.
-    # Check filesize
-    # statinfo = os.stat(filename)
-    # statinfo.st_size == expected_bytes
+    # Verify using file size
+    if v_type.lower() == "bytes":
+        return stat(file).st_size == int(v_val)
 
-    return hash == v_val
+    # Verify using hash
+    else:
+        v_types = {"md5": hashlib.md5,
+                   "sha1": hashlib.sha1,
+                   "sha256": hashlib.sha256,
+                   "sha512": hashlib.sha512
+                   }
+        with open(file, 'rb') as fileObj:
+            content = fileObj.read()
+        hash = v_types[v_type.lower()](content).hexdigest()
 
+        return hash == v_val
 
 
 
