3920f62d46d2ad4ed6e3e22dd461e031346c2bcf
ronrest <ronny.coding@gmail.com>
2016-06-05 15:38:26 +1000

    NLP - Add docstring to sentence_2_freq_vec()

--- convenience/nlp/sentence_2_freq_vec.py
+++ convenience/nlp/sentence_2_freq_vec.py
@@ -1,7 +1,25 @@
 import collections
 
 
+# ==============================================================================
+#                                                            SENTENCE_2_FREQ_VEC
+# ==============================================================================
 def sentence_2_freq_vec(s, vocab_list):
+    """
+    Given a list of words from a sentence, and a vocabulary list that specifies
+    the order of the different words for the final output vector, it returns
+    a list of integers representing a vector encoding of the word frequency
+    counts.
+
+    :param s: (list of strings)
+        The sentence as a list of words.
+    :param vocab_list: (list of strings)
+        The ordered vocabulary as a list of words.
+    :return:(list of integers)
+        The frequency counts
+
+    """
+    # ==========================================================================
     # TODO: compare the perfomrance of different methods of achieving this
     #       same task.
 
