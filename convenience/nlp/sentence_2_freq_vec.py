60ebf591fa76bd371ab1b6a9337eff8f5e4419ce
ronrest <ronny.coding@gmail.com>
2016-06-05 16:01:09 +1000

    NLP - Added a threshold value to the onehot encoding for sentence_2_freq_vec()

--- convenience/nlp/sentence_2_freq_vec.py
+++ convenience/nlp/sentence_2_freq_vec.py
@@ -4,7 +4,7 @@ import collections
 # ==============================================================================
 #                                                            SENTENCE_2_FREQ_VEC
 # ==============================================================================
-def sentence_2_freq_vec(s, vocab_list, ohv=False):
+def sentence_2_freq_vec(s, vocab_list, ohv=False, thresh=1):
     """
     Given a list of words from a sentence, and a vocabulary list that specifies
     the order of the different words for the final output vector, it returns
@@ -19,6 +19,13 @@ def sentence_2_freq_vec(s, vocab_list, ohv=False):
         Should it return as a one hot vector?
         True:  Any word occuring at least once gets a value of 1
         False  (default) the values in the vector will be word counts in s
+    :param: thresh: (int) (default=1)
+        If using one hot encoding (`ohv`=True), use this variable to set the
+        threshold of how many times a word must occur as a minumum in the
+        sentence for it gets assigned a value of 1.
+
+        Default = 1 (word must occur at least once to get a value of 1)
+
     :return:(list of integers)
         The frequency counts
 
@@ -30,6 +37,9 @@ def sentence_2_freq_vec(s, vocab_list, ohv=False):
 
         >>> sentence_2_freq_vec(s, vocab_list=vocab, ohv=True)
         [1, 0, 1, 1, 1, 0, 0]
+
+        >>> sentence_2_freq_vec(s, vocab_list=vocab, ohv=True, thresh=2)
+        [0, 0, 1, 1, 0, 0, 0]
     """
     # ==========================================================================
     # TODO: compare the perfomrance of different methods of achieving this
@@ -37,7 +47,7 @@ def sentence_2_freq_vec(s, vocab_list, ohv=False):
 
     tally = collections.Counter(s)
     if ohv:
-        return [int(tally[word] > 0) for word in vocab_list]
+        return [int(tally[word] >= thresh) for word in vocab_list]
     else:
         return [tally[word] for word in vocab_list]
 
