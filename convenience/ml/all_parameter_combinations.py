00bb593d945973f3c8a90f9aa9340f508997b784
ronrest <ronny.coding@gmail.com>
2016-06-23 21:46:33 +1000

    ML - Add docstring to all_parameter_combinations()

--- convenience/ml/all_parameter_combinations.py
+++ convenience/ml/all_parameter_combinations.py
@@ -1,7 +1,29 @@
 import itertools
 
 
+# ==============================================================================
+#                                                     ALL_PARAMETER_COMBINATIONS
+# ==============================================================================
 def all_parameter_combinations(d):
+    """
+    Takes a dictionary of lists, eg:
+        d = {
+            "type": ["a", "b"],
+            "size": [10, 20]
+           }
+
+    and returns a list of dictionaries that explore all possible combinations of
+    values, eg:
+        [{'size': 10, 'type': 'a'},
+         {'size': 20, 'type': 'a'},
+         {'size': 10, 'type': 'b'},
+         {'size': 20, 'type': 'b'}]
+
+    :param d: (dictionary of lists)
+        The dictionary, of lists of values to explore.
+    :return: (list of dictionaries)
+    """
+    # ==========================================================================
     keys = d.keys()
     values = (d[varName] for varName in keys)
     combinations = [dict(zip(keys, combination)) for combination in
