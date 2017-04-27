4650f9ad08cf01c519d6d169241ee71f338a816e
ronrest <ronny.coding@gmail.com>
2017-04-28 00:13:54 +1000

    DOCS: ML: add docstring to `minmax_scaling()`

--- convenience/ml/feature_scaling.py
+++ convenience/ml/feature_scaling.py
@@ -1,6 +1,19 @@
 import numpy as np
 
 def minmax_scaling(x, min=None, max=None):
+    """
+
+    :param x:
+        the 1D array
+    :param min: (default = None)
+        The min value to use for scaling.
+        If None, then it automatically uses teh min of the array provided
+    :param max:
+        The max value to use for scaling
+        If None, then it automatically uses the max of the array provided
+    :return:
+        An array with all the values scaled
+    """
     x = np.array(x)
     xmax = x.max() if max is None else max
     xmin = x.min() if min is None else min
