51018c39a4121ef31eb997061f05ccbeba09fef1
ronrest <ronny.coding@gmail.com>
2017-04-29 22:03:30 +1000

    FEAT: WEBSCRAPING: add `download_links_from_page()`

---
+++ convenience/webscraping/download_all_links.py
@@ -0,0 +1,70 @@
+
+from BeautifulSoup import BeautifulSoup
+import urllib2
+import re
+import urllib
+import os
+
+def download_links_from_page(page, extension="", output_dir="", ltf=False,
+                             base_url=""):
+    # Creates the directory if it does not already exist
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Automatically detect if the content is a HTTP URL, if so, download the
+    # html page content. otherwise treat 'page' as an html page.
+    # Get the html content from the url specified
+    url_was_provided = True if re.search("^(https://|http://)", page) else False
+    if url_was_provided:
+        html = urllib2.urlopen(page)
+        base_url = os.path.split(page)[0]
+    else:
+        html = page
+
+    soup = BeautifulSoup(html)
+
+    # Returns all links
+    if extension != "":
+        links = soup.findAll('a',
+                             attrs={'href': re.compile(".{}$".format(extension))})
+    else:
+        links = soup.findAll('a')
+
+    # ------------------------------------------------------------------------------
+    #                                                   DOWNLOAD EACH FILE AT A TIME
+    # ------------------------------------------------------------------------------
+    unprocessed_links = []  # keep a list of files that could not be downloaded
+    for link in links:
+        link_url = link["href"]
+
+        # Determine if it's an absolute path to a file  using http or https file
+        full_url = True if re.search("^(https://|http://)", link_url) else False
+
+        # Use base_url to convert to a full path if link_url is a relative path
+        link_url = link_url if full_url else os.path.join(base_url, link_url)
+
+        # --------------------------------------------------------------------------
+        # Extract the link test to use as the file name, otherwise use the remote
+        # filename
+        # --------------------------------------------------------------------------
+        if ltf:
+            filename = (link.contents[0]).replace(" ", "_") + "." + extension
+        else:
+            filename = os.path.split(link_url)[1]
+
+        # --------------------------------------------------------------------------
+        #                                                          Download the file
+        # --------------------------------------------------------------------------
+        try:
+            print("Downloading " + link_url)
+            urllib.urlretrieve(link_url, os.path.join(output_dir, filename))
+            print("--done")
+        except:
+            print("Could NOT download " + link_url)
+            unprocessed_links.append(link_url)
+
+    print("Done Downloading Files")
+    if len(unprocessed_links) > 0:
+        print("The following could NOT be downloaded" + "\n    ".join([""] + a))
+
+# download_links_from_page(url, extension="jpg", output_dir="/tmp/mydir", ltf=False)
