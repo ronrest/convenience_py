bdc8c7fd75a3a75ef644e0efaf55c4e5cf390226
ronrest <ronny.coding@gmail.com>
2017-05-01 22:13:46 +1000

    STYLE: FILE: Change docstring style to google style for `obj2pickle()`

--- convenience/file/obj2pickle.py
+++ convenience/file/obj2pickle.py
@@ -3,24 +3,24 @@ import pickle
 #                                                                     OBJ2PICKLE
 # ==============================================================================
 def obj2pickle(obj, file, protocol=-1):
-    """
-    Saves an object as a binary pickle file to the desired file path.
+    """ Saves an object as a binary pickle file to the desired file path.
+    
+    Args:
+        obj: (object)
+            The python object you want to save.
+        file: (str)
+            File path of file you want to save as.  eg /tmp/myFile.pkl
+        protocol: (int) (default = -1)
+            0  = original ASCII protocol and is backwards compatible with earlier
+                 versions of Python.
+            1  = old binary format which is also compatible with earlier versions
+                 of Python.
+            2  = provides much more efficient pickling of new-style classes.
+                 compatible with Python >= 2.3
+            -1 = uses the highest (latest) protocol available. This is the default
+                 value for this function.
 
-    :param obj: (object)
-        The python object you want to save.
-    :param file: (str)
-        File path of file you want to save as.  eg /tmp/myFile.pkl
-    :param protocol: (int) (default = -1)
-        0  = original ASCII protocol and is backwards compatible with earlier
-             versions of Python.
-        1  = old binary format which is also compatible with earlier versions
-             of Python.
-        2  = provides much more efficient pickling of new-style classes.
-             compatible with Python >= 2.3
-        -1 = uses the highest (latest) protocol available. This is the default
-             value for this function.
-
-    :example:
+    Examples:
         s = "This is my super important object i want to cache"
         obj2pickle(s, "/home/fred/my_cached_object.pkl")
     """
