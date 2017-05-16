63cfc12ed2f936e9801f6200b5c6590f219492f4
ronrest <ronny.coding@gmail.com>
2017-05-16 21:35:49 +1000

    STYLE: FILE: convert docstring in `uncompress()` to google style

--- convenience/file/uncompress.py
+++ convenience/file/uncompress.py
@@ -4,18 +4,17 @@
 #                                                                     UNCOMPRESS
 # ==============================================================================
 def uncompress(file, method="tar"):
-    """
-    Uncompress packaged files such as zip and tarballs.
-
-    :param file: {string}
-        filepath of compressed file
-    :param method: {string}
-        Method of uncompression to use.
-
-        TODO: autodetect based on filename.
-
-        "tar"  = for tar.gz files.
-
+    """ Uncompress packaged files such as zip and tarballs.
+    
+    Args:
+        file: (string)
+            filepath of compressed file
+        method: (string)
+            Method of uncompression to use.
+    
+            TODO: autodetect based on filename.
+    
+            "tar"  = for tar.gz files.
     """
     #TODO: Add an output dir option.
     if method == "tar":
@@ -23,5 +22,4 @@ def uncompress(file, method="tar"):
         with tarfile.open(filename) as fileObj:
             #sys.stdout.flush()
             fileObj.extractall()
-
 
