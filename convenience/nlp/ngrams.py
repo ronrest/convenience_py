168729e30935d030d529c39f5ed88864193f91a5
ronrest <ronny.coding@gmail.com>
2017-05-20 19:46:22 +1000

    DOCS: NLP: Add docstring to `create_ngrams()` function

--- convenience/nlp/ngrams.py
+++ convenience/nlp/ngrams.py
@@ -1,5 +1,23 @@
 
+# ==============================================================================
+#                                                                  CREATE_NGRAMS
+# ==============================================================================
 def create_ngrams(doc, n=2):
+    """ Given a list of words `doc`, it returns a list of ngrams, where
+        each row contains a list of two elements:
+        - A list of the first n-1 words
+        - The final word
+
+    Examples:
+        >>> doc = "once upon a time there lived a giant frog".split()
+        >>> create_ngrams(doc, n=3)
+        [[['once', 'upon'], 'a'],
+        [['upon', 'a'], 'time'],
+        [['a', 'time'], 'there'],
+        [['time', 'there'], 'lived'],
+        [['there', 'lived'], 'a'],
+        [['lived', 'a'], 'giant']]
+    """
     ngrams = [[[doc[j] for j in range(i, i + n - 1)], doc[i + n - 1]] for i in
               range(0, len(doc) - n)]
     return ngrams
