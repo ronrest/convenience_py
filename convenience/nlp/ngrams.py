fd1aa5be0d4fa5f16985ec55771bfcc63ea7f1e5
ronrest <ronny.coding@gmail.com>
2017-06-09 21:27:31 +1000

    FEAT: NLP: Add `create_ngrams_xy()` function

--- convenience/nlp/ngrams.py
+++ convenience/nlp/ngrams.py
@@ -22,3 +22,7 @@ def create_ngrams(doc, n=2):
               range(0, len(doc) - n)]
     return ngrams
 
+def create_ngrams_xy(doc, n=2):
+    x = [[doc[j] for j in range(i, i + n - 1)] for i in range(0, len(doc) - n)]
+    y = [doc[i + n - 1] for i in range(0, len(doc) - n)]
+    return x, y
