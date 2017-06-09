4615b909a005527f6ebe330cd2d2b98e4a799907
ronrest <ronny.coding@gmail.com>
2017-06-09 21:28:28 +1000

    DOCS: NLP: Add docstring for `create_ngrams_xy()` function

--- convenience/nlp/ngrams.py
+++ convenience/nlp/ngrams.py
@@ -22,7 +22,32 @@ def create_ngrams(doc, n=2):
               range(0, len(doc) - n)]
     return ngrams
 
+
+# ==============================================================================
+#                                                               CREATE_NGRAMS XY
+# ==============================================================================
 def create_ngrams_xy(doc, n=2):
+    """ Given a list of words `doc`, it returns two lists, x and y.
+
+        x = A list of lists, with each row being the first n-1 words of the
+            ngram, which can be readily used as the input to a model.
+        y = A list, with element being the nth word in the n gram, which can
+            readily be used as the target label to be predicted.
+
+    Examples:
+        >>> doc = "once upon a time there lived a giant frog".split()
+        >>> x,y = create_ngrams_xy(doc, n=3)
+        >>> print(x)
+        [['once', 'upon'],
+         ['upon', 'a'],
+         ['a', 'time'],
+         ['time', 'there'],
+         ['there', 'lived'],
+         ['lived', 'a']]
+        >>> print(y)
+        ['a', 'time', 'there', 'lived', 'a', 'giant']
+    """
     x = [[doc[j] for j in range(i, i + n - 1)] for i in range(0, len(doc) - n)]
     y = [doc[i + n - 1] for i in range(0, len(doc) - n)]
     return x, y
+
