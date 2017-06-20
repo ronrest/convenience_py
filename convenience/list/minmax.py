bbbedcb09bb662e0934cf23f6a6b453cc1ca0b95
ronrest <ronny.coding@gmail.com>
2017-06-20 22:06:37 +1000

    DOCS: LIST: Add docstring to `argmax()` function

--- convenience/list/minmax.py
+++ convenience/list/minmax.py
@@ -1,4 +1,8 @@
 
+# ==============================================================================
+#                                                                         ARGMAX
+# ==============================================================================
 def argmax(a):
+    """ Given a list `a`, it returns the index of the max value. """
     return max(range(len(a)), key=lambda i: a[i])
 
