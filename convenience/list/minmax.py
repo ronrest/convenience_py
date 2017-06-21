336c8dc012ea2e227bae17e05cfbeaefbdce97de
ronrest <ronny.coding@gmail.com>
2017-06-21 21:50:41 +1000

    FEAT: LIST: Add `argmin()` function

--- convenience/list/minmax.py
+++ convenience/list/minmax.py
@@ -6,3 +6,8 @@ def argmax(a):
     """ Given a list `a`, it returns the index of the max value. """
     return max(range(len(a)), key=lambda i: a[i])
 
+
+def argmin(a):
+    return min(range(len(a)), key=lambda i: a[i])
+
+
