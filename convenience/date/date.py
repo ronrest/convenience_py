24cd140ad77e66982c34fa7637c7a4a3b32a5411
ronrest <ronny.coding@gmail.com>
2017-07-27 11:04:45 +1000

    DOCS: DATE: Add docstring for n_days_ago()

--- convenience/date/date.py
+++ convenience/date/date.py
@@ -1,4 +1,10 @@
 from datetime import datetime, timedelta
 
+# ==============================================================================
+#                                                                     N_DAYS_AGO
+# ==============================================================================
 def n_days_ago(n):
+    """ Given an integer representing number of days, it returns
+        the datetime object that was n number of days ago
+    """
     return datetime.now() - timedelta(days=n)
