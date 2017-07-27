cc5e40afcc697dbe1560b54d20d94988059a984f
ronrest <ronny.coding@gmail.com>
2017-07-27 11:07:05 +1000

    FEAT: DATE: Add num_with_extension()

--- convenience/date/date.py
+++ convenience/date/date.py
@@ -1,6 +1,21 @@
 from datetime import datetime, timedelta
 
 # ==============================================================================
+def num_with_extension(n):
+    if 10 <= n % 100 <= 20:  # special case of 11-13
+        ext = "th"
+    elif n % 10 == 1:
+        ext = "st"
+    elif n % 10 == 2:
+        ext = "nd"
+    elif n % 10 == 3:
+        ext = "rd"
+    else:
+        ext = "th"
+    return "{}{}".format(n, ext)
+
+
+# ==============================================================================
 #                                                                     N_DAYS_AGO
 # ==============================================================================
 def n_days_ago(n):
