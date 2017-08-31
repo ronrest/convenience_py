0547c463cc6860fdd7100d5ace99ed2f2f0c8645
ronrest <ronny.coding@gmail.com>
2017-08-31 19:32:41 +1000

    FIX: made obj2pickle compatible between py2.7 and py3.x by default

--- convenience/file/obj2pickle.py
+++ convenience/file/obj2pickle.py
@@ -4,23 +4,25 @@ import pickle
 # ==============================================================================
 #                                                                     OBJ2PICKLE
 # ==============================================================================
-def obj2pickle(obj, file, protocol=-1):
+def obj2pickle(obj, file, protocol=2):
     """ Saves an object as a binary pickle file to the desired file path.
-    
+
     Args:
         obj: (object)
             The python object you want to save.
         file: (str)
             File path of file you want to save as.  eg /tmp/myFile.pkl
-        protocol: (int) (default = -1)
+        protocol: (int) (default = 2)
             0  = original ASCII protocol and is backwards compatible with earlier
                  versions of Python.
             1  = old binary format which is also compatible with earlier versions
                  of Python.
             2  = provides much more efficient pickling of new-style classes.
-                 compatible with Python >= 2.3
-            -1 = uses the highest (latest) protocol available. This is the default
-                 value for this function.
+                 compatible with Python >= 2.3.
+                 (Use this value to be compatible between py2.7 and py3.x)
+            3 = Python >= 3.x?
+            4 = Python >= 3.4
+            -1 = uses the highest (latest) protocol available.
 
     Examples:
         s = "This is my super important object i want to cache"
