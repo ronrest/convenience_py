854a815222ce1a6b917a36c8966a66049d5ba617
ronrest <ronny.coding@gmail.com>
2017-09-04 22:11:28 +1000

    FEAT: improved version of batch_of_images_to_grid() with new API

--- ml/img/batch_of_images_to_grid.py
+++ ml/img/batch_of_images_to_grid.py
@@ -2,65 +2,47 @@ import numpy as np
 
 
 # ==============================================================================
-#                                                GRID_OF_IMAGES_FROM_BATCH_ARRAY
+#                                                        BATCH_OF_IMAGES_TO_GRID
 # ==============================================================================
-def batch_of_images_to_grid(X, rowcol=(3,2), seed=None):
+def batch_of_images_to_grid(imgs, rows, cols):
     """
-    Given a batch of images (stored as a numpy array), it creates a grid
-    of images arranged in `rowcol=(n_rows, n_cols)` images.
+    Given a batch of images stored as a numpy array of shape:
+        [n_batch, img_height, img_width, n_channels]
+    it creates a grid of images of shape described in `rows` and `cols`.
 
     Args:
-        X: (numpy array)
-            Shape should be either:
-                - [n_batch, im_rows, im_cols]
+        imgs: (numpy array)
+            Shape should be:
                 - [n_batch, im_rows, im_cols, n_channels]
 
-
-        rowcol: (tuple of two ints)(default=(2,2))
-            (rows, cols)
-
-        param seed: (int)
-            Set the random seed.
+        rows: (int) How many rows of images to use
+        cols: (int) How many cols of images to use
 
     Returns: (numpy array)
-        The grid of images as one large image of either shape:
-            - [n_classes*im_cols, num_per_class*im_rows]
+        The grid of images as one large image of shape:
             - [n_classes*im_cols, num_per_class*im_rows, n_channels]
-
     """
+    # TODO: handle case where images have no color chanels (greyscale)
+
     # TODO: have a resize option to rescale the individual sample images
     # Set the random seed if needed
-    if seed is not None:
-        np.random.seed(seed=seed)
 
-    # Dimensions of the grid.
-    rows, cols = rowcol
-    im_shape = X[0].shape
-    im_width = im_shape[1]
-    im_height = im_shape[0]
-    if len(im_shape)>2:
-        n_channels = im_shape[2]
-        grid_shape = (im_height * rows, im_width * cols, n_channels)
-    else:
-        grid_shape = (im_height * rows, im_width * cols)
+    # TODO: Have a random shuffle option
+    # if seed is not None:
+    #     np.random.seed(seed=seed)
 
-    # Initialise the grid array
-    grid_array = np.zeros(grid_shape, dtype=X[0].dtype)
+    # Only use the number of images needed to fill grid
+    n_cells = (rows*cols)
+    imgs = imgs[:n_cells]
 
-    # For each class, sample num_per_class images and place them in grid
-    sample_indices = np.random.choice(range(len(X)), size=min(rows*cols, len(X)), replace=False)
-    print("num samples imdices", len(sample_indices), sample_indices)
-    for i in range(rows):
-        for j in range(cols):
-            row = i*im_height
-            col = j*im_width
+    n_batch, img_height, img_width, n_channels = imgs.shape
 
-            # If not enough data to fill grid, then stop now
-            sample_index = i*cols+j
-            if sample_index >= len(X):
-                break
+    # Handle case where there is not enough images in batch to fill grid
+    n_gap = n_cells - n_batch
+    imgs = np.pad(imgs, pad_width=[(0,n_gap),(0,0), (0,0), (0,0)], mode="constant", constant_values=0)
 
-            # Append to corresponding position on grid
-            grid_array[row:row+im_height, col:col+im_width] = X[sample_indices[sample_index]]
+    # Reshape into grid
+    grid = imgs.reshape(rows,cols,img_height,img_width,n_channels).swapaxes(1,2)
+    grid = grid.reshape(rows*img_height,cols*img_width,n_channels)
 
-    return grid_array
+    return grid
