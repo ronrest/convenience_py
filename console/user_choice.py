32db6c745011e1b5c43cd0bb1f0dc9eb1808428a
ronrest <ronny.coding@gmail.com>
2017-09-05 21:25:18 +1000

    DOCS: Add docstring for user_choice()

--- console/user_choice.py
+++ console/user_choice.py
@@ -1,4 +1,20 @@
+# ==============================================================================
+#                                                                    USER_CHOICE
+# ==============================================================================
 def user_choice(msg, legal_options={"yes", "no"}):
+    """ Prompts for user input in termimal untill user selects one of
+        the legal options from the set.
+
+        Continues prompting the user with same message untill they enter
+        a legal option.
+
+    Args:
+        msg: (str) Message to prompt the user.
+        legal_options: (set of str) The set of acceptable responses
+    Returns: (str)
+        Returns the legal choice that was entered by the user
+        (with whitespaces stripped)
+    """
     choice = ""
     while choice.strip() not in legal_options:
         choice = raw_input(msg)
