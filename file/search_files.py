1849c2c7b4f2518a72836ec581c003c4e9be94fc
ronrest <ronny.coding@gmail.com>
2017-09-05 21:15:34 +1000

    DOCS: Add docstring for search_files()

--- file/search_files.py
+++ file/search_files.py
@@ -2,7 +2,14 @@ import os
 import fnmatch
 
 
+# ==============================================================================
+#                                    RECURSIVELY FIND FILES THAT MATCH A PATTERN
+# ==============================================================================
 def search_files(root, pattern="*.jpg"):
+    """ Search for files that match a pattern recursively through
+        subdirectories. Includes hidden files and things in hidden
+        directories that use the "." notation.
+    """
     matches = []
     for curdir, dirnames, filenames in os.walk(root):
         for filename in fnmatch.filter(filenames, pattern):
