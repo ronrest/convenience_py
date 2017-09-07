3c362a9c195e1bcaf784570020eb72c57cf07018
ronrest <ronny.coding@gmail.com>
2017-09-07 16:57:45 +1000

    FEAT: Add mpl_show_img()

---
+++ ml/img/show_img.py
@@ -0,0 +1,14 @@
+# ==============================================================================
+#                                                                   MPL_SHOW_IMG
+# ==============================================================================
+import matplotlib.pyplot as plt
+def mpl_show_img(a, figsize=(15,10)):
+    """Given a numpy array representing an image, view it (using matplotlib)"""
+    fig, ax = plt.subplots(1, 1, figsize=figsize)
+    plt.imshow(a,  cmap="gray")     # Can actually render RGB with "gray"
+    ax.grid(False)                     # Remove gridline
+    ax.get_yaxis().set_visible(False)  # Remove axis ticks
+    ax.get_xaxis().set_visible(False)  # Remove axis ticks
+    plt.show()
+
+
