3d8a019f9b303b36aa7a73ddfbd4966f9914c4e6
ronrest <ronny.coding@gmail.com>
2017-09-08 00:38:41 +1000

    STYLE: SH: Add decorators to printouts

--- sh/tensorflow_on_new_computer.sh
+++ sh/tensorflow_on_new_computer.sh
@@ -27,18 +27,18 @@ VIRTUAL_ENV_ROOT="${HOME}/virtualenvs" # Where your virtual envs are stored.
 #-------------------------------------------------------------------------------
 START_DIR=pwd   # Store Initial Working Directory
 
-echo "==========================================================="
+echo "\n==========================================================="
 echo "                            INSTALL THE DEVELOPER LIBRARIES"
-echo "==========================================================="
+echo "===========================================================\n"
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get install -y build-essential git  swig
 sudo apt-get install -y python-pip python-dev python-wheel python-virtualenv
 sudo apt-get install -y unzip
 
-echo "==========================================================="
+echo "\n==========================================================="
 echo "                                           SETUP VIRTUALENV"
-echo "==========================================================="
+echo "===========================================================\n"
 echo "CREATING VIRTUALENV"
 mkdir -p ${VIRTUAL_ENV_ROOT}
 cd ${VIRTUAL_ENV_ROOT}
@@ -48,14 +48,16 @@ echo "ENTERING VIRTUALENV"
 . ${VIRTUAL_ENV_ROOT}/${VIRTUAL_ENV_NAME}/bin/activate
 
 
-echo "==========================================================="
+echo "\n==========================================================="
 echo "                                   INSTALL PYTHON LIBRARIES"
-echo "==========================================================="
+echo "===========================================================\n"
 echo "UPGRADING PIP"
 pip install --upgrade pip
 sudo apt-get update
 
+echo "\n==========================================================="
 echo "INSTALLING NUMPY AND SCIPY"
+echo "===========================================================\n"
 sudo apt-get install -y libopenblas-dev  # Speeds up numpy/scipy
 sudo apt-get install -y liblapack-dev gfortran # Needed for scipy/numpy
 pip install numpy
@@ -63,44 +65,56 @@ pip install scipy
 # sudo apt-get install -y python-numpy
 
 
+echo "\n==========================================================="
 echo "INSTALLING PANDAS"
+echo "===========================================================\n"
 sudo apt-get install -y python-tk  # Needed by Pandas
 pip install pytz                   # Needed by pandas
 pip install pandas
 
 # Libraries for image processing
+echo "\n==========================================================="
 echo "INSTALLING PILLOW IMAGE PROCESSING LIBRARY"
+echo "===========================================================\n"
 sudo apt-get install -y libjpeg-dev libpng12-dev    # Needed by pillow
 pip install Pillow
 
+echo "\n==========================================================="
 echo "INSTALLING VISUALIZATION LIBRARIES"
+echo "===========================================================\n"
 pip install pydot
 pip install graphviz
 
+echo "\n==========================================================="
 echo 'INSTALLING PLOTTING LIBRARIES'
+echo "===========================================================\n"
 sudo apt-get install matplotlib
 #pip install -U matplotlib
 pip install seaborn
 
-echo 'INSTALLING ADITIONAL USEFUL PYTHON LIBRARIES'
+echo "\n==========================================================="
+echo 'INSTALLING DATA LOADING LIBRARIES'
+echo "===========================================================\n"
 pip install h5py
 
+echo "\n==========================================================="
 echo 'INSTALL JUPYTER'
+echo "===========================================================\n"
 # Jupyter is upgraded and forced to reinstall on the virtualenv to
 # ensure that the command `jupyter kernelspec list` will recognize
 # the virtualenv as containing a viable python kernel to run
 pip install -U --force-reinstall jupyter
 
 
-echo "==========================================================="
+echo "\n==========================================================="
 echo "                                         INSTALL TENSORFLOW"
-echo "==========================================================="
+echo "===========================================================\n"
 pip install --upgrade ${TF}
 
 
-echo "==========================================================="
+echo "\n==========================================================="
 echo "                                               FINISHING UP"
-echo "==========================================================="
+echo "===========================================================\n"
 echo "EXITING VIRTUALENV"
 deactivate
 
