adc1d8e06449cd4a68e4a2ab349086fd98ddf296
ronrest <ronny.coding@gmail.com>
2017-09-08 17:46:40 +1000

    DOCS: Update docstring for batch_of_images_to_grid()

--- ml/img/batch_of_images_to_grid.py
+++ ml/img/batch_of_images_to_grid.py
@@ -7,19 +7,24 @@ import numpy as np
 def batch_of_images_to_grid(imgs, rows, cols):
     """
     Given a batch of images stored as a numpy array of shape:
-        [n_batch, img_height, img_width, n_channels]
-    it creates a grid of images of shape described in `rows` and `cols`.
+
+           [n_batch, img_height, img_width]
+        or [n_batch, img_height, img_width, n_channels]
+
+    it creates a grid of those images of shape described in `rows` and `cols`.
 
     Args:
         imgs: (numpy array)
-            Shape should be:
+            Shape should be either:
+                - [n_batch, im_rows, im_cols]
                 - [n_batch, im_rows, im_cols, n_channels]
 
         rows: (int) How many rows of images to use
         cols: (int) How many cols of images to use
 
     Returns: (numpy array)
-        The grid of images as one large image of shape:
+        The grid of images as one large image of either shape:
+            - [n_classes*im_cols, num_per_class*im_rows]
             - [n_classes*im_cols, num_per_class*im_rows, n_channels]
     """
     # TODO: have a resize option to rescale the individual sample images
