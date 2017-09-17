41a37c2878629f3596a3663c39ad6b53c6c748d4
ronrest <ronny.coding@gmail.com>
2017-09-17 15:12:51 +1000

    FEAT: ML: Add basic tensorflow workflow

---
+++ ml/tf/workflow_basic.py
@@ -0,0 +1,24 @@
+""" The most basic workflow for testing components of tensorflow. 
+"""
+from __future__ import print_function, division
+import numpy as np
+import tensorflow as tf
+
+graph = tf.Graph()
+with graph.as_default():
+    tf_X = tf.placeholder(tf.float32, shape=(3, 4))
+    tf_y = tf_X * 2
+
+with tf.Session(graph=graph) as sess:
+    X = np.random.randn(3,4)
+    output = sess.run(tf_y, feed_dict={tf_X: X})
+    print(output)
+
+
+# # INTERACTIVE MODE
+# sess = with tf.Session(graph=graph):
+# X = np.random.randn(3,4)
+# output = session.run(tf_y, feed_dict={tf_X: X})
+# print(output)
+#
+# sess.close()
