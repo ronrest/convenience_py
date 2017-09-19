572e0231e53bd27cad28a8b074b8e44b84b08148
ronrest <ronny.coding@gmail.com>
2017-09-19 22:39:34 +1000

    FEAT: ML: Add random_crop()

--- ml/img/image_processing.py
+++ ml/img/image_processing.py
@@ -22,3 +22,20 @@ def array2pil(a, mode="RGB"):
     return Image.fromarray(a, mode=mode)
 
 
+def random_crop(im, min_scale=0.5, preserve_size=False, resample=PIL.Image.NEAREST):
+    if min_scale == 0:
+        return im
+    else:
+        width, height = np.array(np.shape(im)[:2])
+        crop_width = np.random.randint(width*min_scale, width)
+        crop_height = np.random.randint(height*min_scale, height)
+        x_offset = np.random.randint(0, width - crop_width + 1)
+        y_offset = np.random.randint(0, height - crop_height + 1)
+        im2 = im.crop((x_offset, y_offset,
+                       x_offset + crop_width,
+                       y_offset + crop_height))
+        if preserve_size:
+            im2 = im2.resize(im.size, resample=resample)
+        return im2
+
+
