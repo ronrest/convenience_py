162b4a868858044fc3d57bcee095327df78e6682
ronrest <ronny.coding@gmail.com>
2017-09-21 23:56:23 +1000

    DOCS: ML: Add docstring for overlayed_single_category_label_image()

--- ml/viz.py/segmentation.py
+++ ml/viz.py/segmentation.py
@@ -8,9 +8,20 @@ __license__ = "Apache License"
 __version__ = "2.0"
 
 
+# ==============================================================================
+#                                          OVERLAYED_SINGLE_CATEGORY_LABEL_IMAGE
+# ==============================================================================
 import PIL
 import numpy as np
 def overlayed_single_category_label_image(X, Y):
+    """ For the task of classifying a single class.
+        Given an image of the scene, and an image of the label,
+        where the class of interest is labelled as a 1 on a 2d
+        image with no color chanels axis. It returns a PIL image
+        of the label overlayed on top of the scene image.
+
+        Images can be either numpy arrays, or PIL image objects.
+    """
     # Ensure images correct type
     if type(X) == PIL.Image.Image:
         img = X
