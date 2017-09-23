3efc13351ac3e67611992a359c2b7caf49a7f50f
ronrest <ronny.coding@gmail.com>
2017-09-23 20:36:57 +1000

    DOCS: ML: Add docstring for resize_batch_of_images()

--- ml/img/resize_batch_of_images.py
+++ ml/img/resize_batch_of_images.py
@@ -1,5 +1,14 @@
+# ==============================================================================
+#                                                         RESIZE_BATCH_OF_IMAGES
+# ==============================================================================
 import scipy.misc
 def resize_batch_of_images(x, size=[32, 32], resample="bicubic"):
+    """ Given an array that is a batch of images, it resizes the
+        images to the desired size [width, height]. You can set
+        the resampling interpolation method as one of:
+
+        ‘nearest’, ‘lanczos’, ‘bilinear’, ‘bicubic’ or ‘cubic’
+    """
     width, height = size
     n_batch, _, _, channels = x.shape
     new_batch = np.zeros([n_batch, height, width, channels], dtype=np.uint8)
