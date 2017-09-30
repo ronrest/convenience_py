8fdd73b7b8b907da264eac26ea52eef2bb8f6ce7
ronrest <ronny.coding@gmail.com>
2017-10-01 00:11:43 +1000

    FEAT: Add opencv version of load_image_as_array()

--- ml/img/load_images.py
+++ ml/img/load_images.py
@@ -1,6 +1,7 @@
 # ==============================================================================
 #                                                            LOAD_IMAGE_AS_ARRAY
 # ==============================================================================
+# USING SCIPY (with i think PIL on the backend)
 import scipy.misc
 def load_image_as_array(f, rescale=None):
     """ Given a filepath to an image file, it loads an image as a numpy array.
@@ -10,3 +11,15 @@ def load_image_as_array(f, rescale=None):
         width, height = rescale
         img = scipy.misc.imresize(img, (height,width))
     return img
+
+
+# USING OPEN CV
+def load_image_as_array(f, rescale=None):
+    # TODO: Check the order of the dimensions for resizing in open cv
+    img = cv2.imread(f, cv2.IMREAD_COLOR)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # Convert to RGB
+    if rescale:
+        img = cv2.resize(img, rescale)
+    return img
+
+
