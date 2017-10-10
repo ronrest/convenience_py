6c8aafb3196bd1d5e3889ffcbf57aca0244acd67
ronrest <ronny.coding@gmail.com>
2017-10-10 18:29:10 +1100

    DOCS: ML: Add docstring for viz_sample_augmentations()

--- ml/viz/viz_sample_augmentations.py
+++ ml/viz/viz_sample_augmentations.py
@@ -5,7 +5,32 @@ import numpy as np
 # array2pil()
 # batch2grid()
 
+# ==============================================================================
+#                                                       VIZ_SAMPLE_AUGMENTATIONS
+# ==============================================================================
 def viz_sample_augmentations(X, aug_func, n_images=5, n_per_image=5, saveto=None):
+    """ Given a batch of data X, and Y,  it takes n_images samples, and performs
+        `n_per_image` random transformations for segmentation data on each of
+        those images. It then puts them in a grid to visualize. Grid size is:
+            n_images wide x n_per_image high
+
+    Args:
+        X:          (np array) batch of images
+        aug_func:   (func) function with API `aug_func(X, Y)` that performs
+                    random transformations on the images for segmentation
+                    purposes.
+        n_images:   (int)
+        n_per_image:(int)
+        saveto:     (str or None)
+
+    Returns: (None, or PIL image)
+        If saveto is provided, then it saves teh image and returns None.
+        Else, it returns the PIL image.
+    Examples:
+        samples = viz_sample_seg_augmentations(data["X_train"], data["Y_train"],
+            aug_func=aug_func, n_images=5, n_per_image=5, saveto=None)
+        samples.show()
+    """
     X = X[:n_images]
     grid = []
 
