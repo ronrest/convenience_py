0914b0975abfec641be5de8dbbea68d39ec2bbb8
ronrest <ronny.coding@gmail.com>
2017-10-14 16:47:30 +1100

    FIX: incorrect argument name

--- ml/img/segmentation.py
+++ ml/img/segmentation.py
@@ -33,6 +33,6 @@ def rgb2seglabel(img, colormap, channels_axis=False):
         label = np.zeros([height, width,1], dtype=np.uint8)
     else:
         label = np.zeros([height, width], dtype=np.uint8)
-    for id in range(len(id2label)):
+    for id in range(len(colormap)):
         label[np.all(img==np.array(idcolormap[id]), axis=2)] = id
     return label
