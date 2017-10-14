ce515265a2052e4c16a5a2add17427b0e9830c50
ronrest <ronny.coding@gmail.com>
2017-10-14 13:41:51 +1100

    FEAT: Add rgb2seglabel()

--- ml/img/segmentation.py
+++ ml/img/segmentation.py
@@ -13,3 +13,12 @@ def pixels_with_value(img, val):
     return np.all(img==np.array(val), axis=2)
 
 
+def rgb2seglabel(img, colormap, channels_axis=False):
+    height, width, _ = img.shape
+    if channels_axis:
+        label = np.zeros([height, width,1], dtype=np.uint8)
+    else:
+        label = np.zeros([height, width], dtype=np.uint8)
+    for id in range(len(id2label)):
+        label[np.all(img==np.array(idcolormap[id]), axis=2)] = id
+    return label
