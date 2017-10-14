8dcd06d0b679b7e94531aad98da3cc41a3e03763
ronrest <ronny.coding@gmail.com>
2017-10-14 20:17:26 +1100

    FEAT: Add rgb2hex()

--- img/color_conversions.py
+++ img/color_conversions.py
@@ -29,3 +29,23 @@ def hex2rgb(s):
         return int(s[0]+s[0], 16), int(s[1]+s[1], 16), int(s[2]+s[2], 16), int(s[3]+s[3], 16)
     elif len(s) == 8:
         return int(s[:2], 16), int(s[2:4], 16), int(s[4:6], 16), int(s[6:8], 16)
+
+
+# ==============================================================================
+#                                                                        RGB2HEX
+# ==============================================================================
+def rgb2hex(code, hash=True, upper=True):
+    """ Given an iterable of integers representing an RGB or RGBA color
+        it returns the hexadecimal version of that color:
+
+    Examples:
+        >>> rgb2hex([7, 33, 255])
+        '#0721FF'
+        >>> rgb2hex([7, 33, 255], hash=False, upper=False)
+        '0721ff'
+    """
+    prefix =  "#" if hash else ""
+    hx = prefix + "".join(["{:>02s}".format(hex(channel)[2:])  for channel in code])
+    if upper:
+        hx = hx.upper()
+    return hx
