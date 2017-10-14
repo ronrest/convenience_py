577549600fe1b50bef048a6c8a93377002710aa9
ronrest <ronny.coding@gmail.com>
2017-10-14 13:42:08 +1100

    DOCS: Add docstring for rgb2seglabel()

--- ml/img/segmentation.py
+++ ml/img/segmentation.py
@@ -13,7 +13,15 @@ def pixels_with_value(img, val):
     return np.all(img==np.array(val), axis=2)
 
 
+# ==============================================================================
+#                                                                   RGB2SEGLABEL
+# ==============================================================================
 def rgb2seglabel(img, colormap, channels_axis=False):
+    """ Given an RGB image stored as a numpy array, and a colormap that
+        maps from label id to an RGB color for that label, it returns a
+        new numpy array with color chanel size of 1 where the pixel
+        intensity values represent the class label id.
+    """
     height, width, _ = img.shape
     if channels_axis:
         label = np.zeros([height, width,1], dtype=np.uint8)
