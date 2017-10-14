e8b3d879590adea5d2fc136214adaeee55c8b12b
ronrest <ronny.coding@gmail.com>
2017-10-14 13:40:51 +1100

    DOCS: Add docstring for pixels_with_value()

--- ml/img/segmentation.py
+++ ml/img/segmentation.py
@@ -1,7 +1,15 @@
 import numpy as np
 
 
+# ==============================================================================
+#                                                              PIXELS_WITH_VALUE
+# ==============================================================================
 def pixels_with_value(img, val):
+    """ Given an image as a numpy array, and a value representing the
+        pixel values, eg [128,255,190] in an RGB image, then it returns
+        a 2D boolean array with a True for every pixel position that has
+        that value.
+    """
     return np.all(img==np.array(val), axis=2)
 
 
