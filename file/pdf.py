6f9e37d03566c8a92a3a9beb2bab893fa7fba8a8
ronrest <ronny.coding@gmail.com>
2017-10-15 22:02:04 +1100

    DOCS: Add docstring for pdf2split_html()

--- file/pdf.py
+++ file/pdf.py
@@ -1,8 +1,21 @@
 from __future__ import print_function, division, unicode_literals
 
+# ==============================================================================
+#                                                                 PDF2SPLIT_HTML
+# ==============================================================================
 import wand.image
 import os
 def pdf2split_html(pdf, saveto, left=0, right=0, top=0, bottom=0, res=100):
+    """ Given a filepath to a pdf document that has two columns of text,
+        and a (preferably not yet created or empty) directory to save
+        the output files to, it does the following:
+
+        - Creates the output directory
+        - Splits all the pages in the original pdf document in half, and
+          creates the left and right images for all pages.
+        - creates an html file (index.html), which embeds all the columns one
+          on top of the other in a continuous document. 
+    """
     print("- Opening pdf file: ", pdf)
     with(wand.image.Image(filename=pdf, resolution=res)) as document:
         print("- getting pages")
