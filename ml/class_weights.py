5855b69f15ac9c316b24358185f18bfe6df8c689
ronrest <ronny.coding@gmail.com>
2017-11-07 18:29:44 +1100

    DOCS: Add docstring for calculate_class_weights()

--- ml/class_weights.py
+++ ml/class_weights.py
@@ -1,6 +1,40 @@
 import numpy as np
 
+# ==============================================================================
+#                                                        CALCULATE_CLASS_WEIGHTS
+# ==============================================================================
 def calculate_class_weights(Y, n_classes, method="paszke", c=1.02):
+    """ Given the training data labels Calculates the class weights.
+
+    Args:
+        Y:      (numpy array) The training labels as class id integers.
+                The shape does not matter, as long as each element represents
+                a class id (ie, NOT one-hot-vectors).
+        n_classes: (int) Number of possible classes.
+        method: (str) The type of class weighting to use.
+
+                - "paszke" = use the method from from Paszke et al 2016
+                            `1/ln(c + class_probability)`
+                - "eigen"  = use the method from Eigen & Fergus 2014.
+                             `median_freq/class_freq`
+                             where `class_freq` is based only on images that
+                             actually contain that class.
+                - "eigen2" = Similar to `eigen`, except that class_freq is
+                             based on the frequency of the class in the
+                             entire dataset, not just images where it occurs.
+                -"logeigen2" = takes the log of "eigen2" method, so that
+                            incredibly rare classes do not completely overpower
+                            other values.
+        c:      (float) Coefficient to use, when using paszke method.
+
+    Returns:
+        weights:    (numpy array) Array of shape [n_classes] assigning a
+                    weight value to each class.
+
+    References:
+        Eigen & Fergus 2014: https://arxiv.org/abs/1411.4734
+        Paszke et al 2016: https://arxiv.org/abs/1606.02147
+    """
     # CLASS PROBABILITIES - based on empirical observation of data
     ids, counts = np.unique(Y, return_counts=True)
     n_pixels = Y.size
