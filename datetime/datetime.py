853135b61f34ece1363da9b53244e775a2ba16a8
ronrest <ronny.coding@gmail.com>
2017-11-08 23:39:35 +1100

    DOCS: Add docstring for convert_timezone()

--- datetime/datetime.py
+++ datetime/datetime.py
@@ -7,9 +7,25 @@ def timestamp2str(t, pattern="%Y-%m-%d  %H:%M:%S"):
     """ Given a float timestamp it returns the date as a formatted string,
         based on the date `pattern` specified """
     return datetime.datetime.fromtimestamp(t).strftime(pattern)
+
+# ==============================================================================
+#                                                               CONVERT_TIMEZONE
+# ==============================================================================
 # import datetime
 from dateutil import tz
 def convert_timezone(time, a="UTC", b="local"):
+    """ Given a datetime object, in timezone a, it changes it to timezone b.
+
+    Args:
+        time:   (datetime object)
+        a:      (str) timezone code to set the from time as.
+                eg:
+                "UTC"
+                "Australia/Melbourne"
+                or..
+                "local"
+        b:      (str) timezone to set the to time as.
+    """
     # TIMEZONE OBJECTS
     tza = tz.tzlocal(a) if (a=="local") else tz.gettz(a)
     tzb = tz.tzlocal(b) if (b=="local") else tz.gettz(b)
