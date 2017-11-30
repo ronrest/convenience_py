2a74026c7f1f8b1a0ebf5679b7292a71c69a4f6c
ronrest <ronny.coding@gmail.com>
2017-11-30 19:54:00 +1100

    FEAT: ML: Add corners2cw()

--- ml/img/bounding_boxes.py
+++ ml/img/bounding_boxes.py
@@ -27,3 +27,30 @@ def cw2corners(boxes):
 
     return out
 
+
+# ==============================================================================
+#                                                                     CORNERS2CW
+# ==============================================================================
+def corners2cw(boxes):
+    """
+    Given an array of boxes that are in [x,y,w,h] (center, width) format, it
+    returns the boxes in [x1,y1,x2,y2] (corners) format.
+
+    It works for both boxes represented as normalized floats between 0-1, and
+    absolute integer values, SO LONG as the integer representation is stored
+    in an array that has dtype of integer.
+
+    NOTE: the center might occasionally be slightly off for integer boxes.
+    """
+    assert boxes.ndim <= 2, "boxes is wrong number of dimensions, should be 2d array"
+    if boxes.ndim == 1:
+        boxes = np.expand_dims(boxes, axis=0)
+    assert boxes.shape[1] == 4, "Boxes must be shape [n_boxes, 4]"
+    out = np.zeros_like(boxes)
+    out[:,2] = boxes[:,2] - boxes[:,0]   # width
+    out[:,3] = boxes[:,3] - boxes[:,1]   # height
+    out[:,0] = boxes[:,0] + (out[:,2]/2.) # x
+    out[:,1] = boxes[:,1] + (out[:,3]/2.) # y
+    return out
+
+
