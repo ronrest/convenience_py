0ce1fb545ad8ca03a6059c02eb99413e205ac7c2
ronrest <ronny.coding@gmail.com>
2017-12-27 18:19:08 +1100

    DOCS: Add docstring for viz_sample_segmentation_augmentations()

--- ml/viz/viz_sample_augmentations.py
+++ ml/viz/viz_sample_augmentations.py
@@ -60,7 +60,39 @@ def viz_sample_augmentations(X, aug_func, n_images=5, n_per_image=5, saveto=None
 
 
 # ==============================================================================
+#                                          VIZ_SAMPLE_SEGMENTATION_AUGMENTATIONS
+# ==============================================================================
 def viz_sample_segmentation_augmentations(X, Y, aug_func, colormap, n_images=5, n_per_image=5, saveto=None):
+    """ Given a batch of data X, and Y,  it takes n_images samples, and performs
+        `n_per_image` random transformations for segmentation data on each of
+        those images. It then puts them in a grid to visualize. Grid size is:
+            2n_images wide x n_per_image high,
+        The segmentation labels appear to the right of each image.
+
+    Args:
+        X:          (np array) batch of images
+        Y:          (np array) batch of labels
+        aug_func:   (func) function with API `aug_func(X, Y)` that performs
+                    random transformations on the images for segmentation
+                    purposes.
+        colormap:   (list of 3-tuples of ints)
+                    A list where each index represents the RGB value
+                    for the corresponding class id.
+                    Eg: to map class_0 to black and class_1 to red:
+                        [(0,0,0), (255,0,0)]
+
+        n_images:   (int) Number of base images to use for visualization
+        n_per_image:(int) Number of augmentations to show per base image
+        saveto:     (str or None)
+
+    Returns: (None, or PIL image)
+        If saveto is provided, then it saves teh image and returns None.
+        Else, it returns the PIL image.
+    Examples:
+        samples = viz_sample_seg_augmentations(data["X_train"], data["Y_train"],
+            aug_func=aug_func, n_images=5, n_per_image=5, saveto=None)
+        samples.show()
+    """
     X = X[:n_images]
     Y = Y[:n_images]
     grid = []
