95079ecd6ddc36b886b517411b460e5cc6ae389a
ronrest <ronny.coding@gmail.com>
2018-01-06 18:30:03 +1100

    FEAT: ML: cv version of image_processing

---
+++ ml/tf/workflow_image_classifier/image_processing_cv.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+randint = np.random.randint
+
+__author__ = "Ronny Restrepo"
+__copyright__ = "Copyright 2018, Ronny Restrepo"
+__credits__ = ["Ronny Restrepo"]
+__license__ = "Apache License"
+__version__ = "2.0"
+
+# import PIL
+# from PIL import Image
+# x = cv2.imread("/home/ronny/cat.jpg")
+# x = cv2.resize(x, (100,100))
+# y = cv2.warpAffine(x, m, x.shape[:2])
+# PIL.Image.fromarray(y).show()
+# PIL.Image.fromarray(y).show()
+
