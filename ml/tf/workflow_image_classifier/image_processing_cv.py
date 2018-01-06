76111df78c3f427759a3bb32ff308036583b18b8
ronrest <ronny.coding@gmail.com>
2018-01-06 18:41:57 +1100

    FEAT: ML: cv version of random_crop()

--- ml/tf/workflow_image_classifier/image_processing_cv.py
+++ ml/tf/workflow_image_classifier/image_processing_cv.py
@@ -22,3 +22,31 @@ __version__ = "2.0"
 def batch_process(X, shape=None, mode=None):
     # TODO: currently does nothing
     return X
+
+
+# ==============================================================================
+#                                                                    RANDOM_CROP
+# ==============================================================================
+def random_crop(im, min_scale=0.5, max_scale=1.0, preserve_size=False):
+    """
+    Args:
+        im:          (numpy array) input image
+        min_scale:   (float) minimum ratio along each dimension to crop from.
+        max_scale:   (float) maximum ratio along each dimension to crop from.
+        preserve_size: (bool) Should it resize back to original dims?
+
+    Returns:
+        (numpy array) randomly cropped image.
+    """
+    assert (min_scale < max_scale), "min_scale MUST be smaller than max_scale"
+    width, height = im.shape[:2]
+    crop_width = np.random.randint(width*min_scale, width*max_scale)
+    crop_height = np.random.randint(height*min_scale, height*max_scale)
+    x_offset = np.random.randint(0, width - crop_width + 1)
+    y_offset = np.random.randint(0, height - crop_height + 1)
+
+    im2 = im[y_offset:y_offset+crop_height,
+             x_offset:x_offset+crop_width]
+    if preserve_size:
+        im2 = cv2.resize(im2, tuple(im.shape[:2]))
+    return im2
