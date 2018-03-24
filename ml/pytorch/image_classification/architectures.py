a879a5a293b65d405e73163b613b7d4be8e10d67
ronrest <ronny.coding@gmail.com>
2018-03-24 13:10:48 +1100

    FEAT: Add basic network to pytorch workflow

--- ml/pytorch/image_classification/architectures.py
+++ ml/pytorch/image_classification/architectures.py
@@ -57,3 +57,44 @@ def fc(fin, out, bn=True, bias=False, dropout=None, activation=nn.ReLU):
     return sq
 
 
+################################################################################
+#                                   NETWORKS
+################################################################################
+class BasicNet(nn.Module):
+    def __init__(self, img_shape, n_classes):
+        """
+        img_shape: (3-tuple) (height, width, n_channels)
+        """
+        super(BasicNet, self).__init__()
+        # SETTINGS
+        conv_activation = nn.ReLU
+        fc_activation = nn.ReLU
+        conv_dropout=0.3
+        fc_dropout = 0.5
+        final_conv_filters = 32
+
+        n_downsamples = 2
+        divisor = (2**n_downsamples)
+        H, W, C = img_shape
+        n_flattened= (W//divisor) * (H//divisor) * final_conv_filters
+
+        # CONV LAYERS
+        trunk = nn.Sequential()
+        trunk.add_module("conv1", conv(3, 16, k=3, s=1, d=1, bn=True, bias=False, dropout=conv_dropout, activation=conv_activation))
+        trunk.add_module("mp1", nn.MaxPool2d(kernel_size=2, stride=2, padding=0))
+        trunk.add_module("conv2", conv(16, final_conv_filters, k=3, s=1, d=1, bn=True, bias=False, dropout=conv_dropout, activation=conv_activation))
+        trunk.add_module("mp2", nn.MaxPool2d(kernel_size=2, stride=2, padding=0))
+        self.trunk = trunk
+
+        # FC LAYERS
+        head = nn.Sequential()
+        # head.add_module("flatten", Flatten())
+        head.add_module("fc1", fc(n_flattened, 128, bn=True, bias=False, dropout=fc_dropout, activation=fc_activation))
+        head.add_module("fc2", fc(128, n_classes, bn=False, bias=True, dropout=None, activation=None))
+        self.head = head
+
+    def forward(self, x):
+        x = self.trunk(x)
+        x = flatten(x)
+        x = self.head(x)
+        return x
