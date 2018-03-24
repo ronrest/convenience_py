dea9632034b462c204478b8f8ef9d92053bb5d1d
ronrest <ronny.coding@gmail.com>
2018-03-24 13:35:02 +1100

    FEAT: Add fit() method to pytorch ImageClassifier class

--- ml/pytorch/image_classification/image_classifier.py
+++ ml/pytorch/image_classification/image_classifier.py
@@ -50,3 +50,33 @@ class ImageClassifier(object):
         self.opt_args["lr"] = lr
         self.optimizer = self.opt_func(self.net.parameters(), **self.opt_args)
 
+    def fit(self, train_gen, valid_gen, n_epochs, steps_per_epoch, valid_steps=100, print_every=100):
+        for epoch in range(n_epochs):
+            running_loss = 0.0
+            running_correct = 0.0
+            running_samples = 0.0
+            for i in range(steps_per_epoch):
+                # Get the batch of data  and Prerpocess and wrap them in Variable
+                X_batch, Y_batch = next(train_gen)
+                # X_batch, Y_batch = preprocess_func(X_batch, Y_batch)
+                X_batch, Y_batch = Variable(torch.Tensor(X_batch)), Variable(torch.LongTensor(Y_batch))
+
+                # Training steps
+                self.optimizer.zero_grad() # zero the parameter gradients
+                logits = self.net(X_batch)
+                _, preds = torch.max(logits, dim=1)
+                loss = self.loss_func(logits, Y_batch)
+                loss.backward()
+                self.optimizer.step()
+
+                # print statistics
+                running_loss += loss.data[0]
+                running_correct += n_correct(preds=preds.data.numpy(), labels=Y_batch.data.numpy())
+                running_samples += len(Y_batch)
+                if i>0 and i % print_every == 0:
+                    avg_loss = running_loss / print_every
+                    avg_acc = running_correct / (running_samples)
+                    running_loss = 0.0
+                    running_correct = 0.0
+                    running_samples = 0.0
+        print('Finished Training')
